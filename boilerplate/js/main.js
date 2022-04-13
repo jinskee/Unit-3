115e9f7d13bf29c548f91c5cee43cb22538f67a3
jinskee <98485932+jinskee@users.noreply.github.com>
2022-04-12 21:26:34 -0500

    Activity 10

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,7 +1,7 @@
 //Wrap everything is immediately invoked anonymous function so nothing is in global scope
 (function (){
     
-    var attrArray = ["BLM_acres", "NPS_acres", "NWR_acres", "State_acres", "USFS_acres"]; //List of attributes 
+    var attrArray = ["NWR_acres", "BLM_acres", "USFS_acres", "NPS_acres", "USFS_acres"]; //List of attributes 
     var expressed = attrArray[0]; //initial attribute
     
     // Begin script when window is loaded
@@ -10,7 +10,7 @@
     //Set up choropleth map 
     function setMap(){
         //map frame dimensions
-        var width = 960,
+        var width = window.innerWidth * 0.5,
             height = 460;//Set up choropleth map
     
 
@@ -70,6 +70,9 @@
             var colorScale = makeColorScale(csvData);
 
             setEnumerationUnits(states,map,path,colorScale);
+
+            //add coordinated visualization to the map
+        	setChart(csvData, colorScale);
 
         };
 
@@ -176,6 +179,78 @@
                 }
                 //return colorScale(d.properties[expressed]);
             });
+    };
+
+    //function to create coordinated bar chart
+    function setChart(csvData, colorScale){
+        //chart frame dimensions
+        var chartWidth = window.innerWidth * 0.425,
+            chartHeight = 460;
+
+        //create a second svg element to hold the bar chart
+        var chart = d3.select("body")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("class", "chart");
+
+        var yScale = d3.scaleLinear()
+            .range([0, chartHeight])
+            .domain([0, 105]);
+        
+        //set bars for each province
+        var bars = chart.selectAll(".bars")
+            .data(csvData)
+            .enter()
+            .append("rect")
+            .sort(function(a, b){
+                return a[expressed]-b[expressed]
+            })
+            .attr("class", function(d){
+                return "bars " + d.STATE_FIPS;
+            })
+            .attr("width", chartWidth / csvData.length - 1)
+            .attr("x", function(d, i){
+                return i * (chartWidth / csvData.length);
+            })
+            .attr("height", function(d){
+                return yScale(parseFloat(d[expressed]));
+            })
+            .attr("y", function(d){
+                return chartHeight - yScale(parseFloat(d[expressed]));
+            }).style("fill", function(d){
+                return colorScale(d[expressed]);
+            });
+
+        //annotate bars with attribute value text
+        var numbers = chart.selectAll(".numbers")
+            .data(csvData)
+            .enter()
+            .append("text")
+            .sort(function(a, b){
+                return a[expressed]-b[expressed]
+            })
+            .attr("class", function(d){
+                return "numbers " + d.STATE_FIPS;
+            })
+            .attr("text-anchor", "middle")
+            .attr("x", function(d, i){
+                var fraction = chartWidth / csvData.length;
+                return i * fraction + (fraction - 1) / 2;
+            })
+            .attr("y", function(d){
+                return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+            })
+            .text(function(d){
+                return d[expressed];
+            });
+
+            //below Example 2.8...create a text element for the chart title
+        var chartTitle = chart.append("text")
+            .attr("x", 20)
+            .attr("y", 40)
+            .attr("class", "chartTitle")
+            .text("Number of " + attrArray[0] + " in each region");
     };
 })();
 /*            var attrArray = ["BLM_acres", "NPS_acres", "NWR_acres", "State_acres", "USFS_acres"];
